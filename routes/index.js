20fafc27ed01c74723d1852df0c98e08c25c3f9d
Bhavin Mistry <bmistry1@stevens.edu>
2022-09-22 00:12:12 -0400

    adding handlebars, updated css links, finalized startup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const aboutRoutes = require("./about");
+const path = require('path');
 
 const constructorMethod = app => {
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve('static/report_bug.html'));
+    });
+
+
     app.use("/about", aboutRoutes);
 
     app.use("*", (req, res) => {
