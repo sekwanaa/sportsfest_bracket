8f6ffac31a5518f921aacad87bd9150d7e736021
sekwanaa <sk123rulz@gmail.com>
2022-09-25 18:17:06 -0400

    created input score partial

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const aboutRoutes = require("./about");
 const reportBugRoutes = require("./report_bugs");
 const teamInputRoutes = require("./team_input");
 const bracketViewRoutes = require("./bracket_view");
+const scoreInputRoutes = require("./score_input");
 const path = require('path');
 
 const constructorMethod = app => {
@@ -16,6 +17,7 @@ const constructorMethod = app => {
     app.use("/report_bug", reportBugRoutes);
     app.use("/team_input", teamInputRoutes);
     app.use("/bracket_view", bracketViewRoutes);
+    app.use("/score_input", scoreInputRoutes);
 
     app.use("*", (req, res) => {
         res.status(404).json({error: "Not found"});
