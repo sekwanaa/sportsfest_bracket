230647565d4df5e4bc741fa7796e8887f56906b5
sekwanaa <sk123rulz@gmail.com>
2022-10-13 13:59:56 -0400

    added env file for mongo login - added .gitignore

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
-const settings = require("./settings");
-const mongoConfig = settings.mongoConfig;
+const serverURI = process.env.serverURI;
+const database = process.env.database
+console.log(serverURI)
+console.log(database)
+// const settings = require("./settings");
+// const mongoConfig = settings.mongoConfig;
 
 let _connection = undefined;
 let _db = undefined;
@@ -8,11 +13,11 @@ let _db = undefined;
 module.exports = {
   dbConnection: async () => {
     if (!_connection) {
-      _connection = await MongoClient.connect(mongoConfig.serverUrl, {
+      _connection = await MongoClient.connect(`${serverURI}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       });
-      _db = await _connection.db(mongoConfig.database);
+      _db = await _connection.db(`${database}`);
     }
 
     return _db;
