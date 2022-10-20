464342ea9c9ea0db2017c846712bf5edf37ddeea
sekwanaa <sk123rulz@gmail.com>
2022-10-20 16:32:13 -0400

    installed Auth0 and played around with the app.js

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const express = require('express');
 const app = express();
 const static = express.static(__dirname + '/public');
 
+const { auth } = require('express-openid-connect');
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'a long, randomly-generated string stored in env',
+    baseURL: 'http://localhost:3000',
+    clientID: 'CmnHPslBTHdjgE08RMzwfjDM70F3ez7L',
+    issuerBaseURL: 'https://dev-e6dfhqdzli1uevwb.us.auth0.com'
+}
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 const configRoutes = require('./routes');
 
 const exphbs = require('express-handlebars');
@@ -10,6 +25,11 @@ const exphbs = require('express-handlebars');
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(auth(config));
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? '/landingPage' : 'Logged out');
+});
 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
