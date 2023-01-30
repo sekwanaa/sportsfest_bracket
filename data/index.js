bef03f11fcb3921bcbdc5f4ed798f7f5f2845b59
bkmistry1 <bmistry1@stevens.edu>
2023-01-30 13:46:45 -0500

    created pool collection and insertion

--- data/index.js
+++ data/index.js
@@ -1,9 +1,11 @@
 const teamData = require("./teamData");
 const userData = require("./userData");
 const matchData = require("./matchData")
+const poolsData = require("./poolsData")
 
 module.exports = {
   teamsData: teamData,
   usersData: userData,
   matchesData: matchData,
+  poolsData: poolsData
 };
