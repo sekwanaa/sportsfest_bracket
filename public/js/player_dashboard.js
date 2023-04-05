0c80e7cd8f930831a8a559139d1a6e7f79347d20
sekwanaa <sk123rulz@gmail.com>
2023-04-05 14:00:09 -0400

    finalized edit profile functionality and routes connection

--- public/js/player_dashboard.js
+++ public/js/player_dashboard.js
@@ -5,20 +5,25 @@
     
     // user profile card stuff
     var editProfileButton = $("#edit_profile_button");
+    // divs which the inputs are in
     var nameUpdateDiv = $("#name_update_div");
     var positionUpdateDiv = $("#position_update_div");
     var shirtNumberUpdateDiv = $("#shirt_number_update_div");
+    // original fields with info
     var playerName = $("#player_name");
     var playerShirtNumber = $("#shirt_number");
     var playerPosition = $("#player_position");
+    var submitProfileChangesButton = $("#submit_profile_changes_button");
     nameUpdateDiv.hide();
     positionUpdateDiv.hide();
     shirtNumberUpdateDiv.hide();
+    submitProfileChangesButton.hide();
     let count = 0;
 
 
     editProfileButton.click(function() {
         count+=1
+        // on second click
         if (count%2==0) {
             playerName.show();
             nameUpdateDiv.hide();
@@ -26,14 +31,42 @@
             positionUpdateDiv.hide();
             playerPosition.show();
             shirtNumberUpdateDiv.hide();
-        } else {
+            submitProfileChangesButton.hide()
+        } else { //on first click
             playerName.hide();
             nameUpdateDiv.show();
             playerShirtNumber.hide();
             positionUpdateDiv.show();
             playerPosition.hide()
             shirtNumberUpdateDiv.show();
+            submitProfileChangesButton.show()
         };
+    });
+
+    submitProfileChangesButton.click(function () {
+        // input fields
+        var playerNameInput = $("#player_name_input").val();
+        var playerShirtNumberInput = $("#shirt_number_input").val();
+        var playerPositionInput = $("#player_position_input").val();
+        try {
+            let req = {
+                method: 'POST',
+                url: '/player_dashboard/submitProfile',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    name: playerNameInput,
+                    shirtNum: playerShirtNumberInput,
+                    position: playerPositionInput,
+                })
+            };
+            $.ajax(req).then(function (res) {
+                console.log("Team Added")
+                console.log("Team ID: " + res);
+            });
+        } 
+        catch (e) {
+            console.log(e)
+        }
     });
 
     // team code input div
