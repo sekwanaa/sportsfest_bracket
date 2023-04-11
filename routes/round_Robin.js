7513abf7f0ffe985563b3075f84f61a2371d22c8
bkmistry1 <bmistry1@stevens.edu>
2023-04-10 22:55:59 -0400

    added missing role handlebar to round robin routes

--- routes/round_Robin.js
+++ routes/round_Robin.js
@@ -40,6 +40,7 @@ router.get("/", async (req, res) => {
             title: "Round-Robin", 
             shortcode: 'roundRobin',
             isAuthenticated: req.oidc.isAuthenticated(),
+            role: userRole,
             rounds: rounds,
             isRounds: isRounds,
         });
