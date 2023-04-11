679d0ced550da624242a03cd13e1f4a2436d0498
bkmistry1 <bmistry1@stevens.edu>
2023-04-11 13:22:41 -0400

    added post route to insert seeds, updated routes to use try-catch blocks

--- routes/seeding_table.js
+++ routes/seeding_table.js
@@ -7,39 +7,57 @@ const teamsData = data.teamsData;
 //bhavin made this change
 router.get("/", async (req, res) => {
 
-    let email = "not authenticated";
-    let loggedInUser = {};
-    let userRole = "";
-    let allUsers = []
-    let matches = null;
+    try {
+        let email = "not authenticated";
+        let loggedInUser = {};
+        let userRole = "";
+        let allUsers = []
+        let matches = null;
+    
+        if(req.oidc.isAuthenticated()) {
+            email = req.oidc.user.name;
+            const user = await userData.getUserByEmail(email);
+            allUsers = await userData.getAllUsers();
+            loggedInUser = user;
+            nickname = loggedInUser.nickname
+            userRole = loggedInUser.user_metadata.role;
+        }
+    
+        const matchHistory = await matchesData.getTeamRecords();
+    
+        res.render("partials/seeding_table", {
+            title: "Seeding Table", 
+            shortcode: 'seedingTable',
+            isAuthenticated: req.oidc.isAuthenticated(),
+            loggedInUser: loggedInUser,
+            role: userRole,
+            allUsers: allUsers,
+            length: allUsers.length,
+            matches: matchHistory,
+            seedNumber: 0,
+        });
+    } catch (e) {
 
-    if(req.oidc.isAuthenticated()) {
-        email = req.oidc.user.name;
-        const user = await userData.getUserByEmail(email);
-        allUsers = await userData.getAllUsers();
-        loggedInUser = user;
-        nickname = loggedInUser.nickname
-        userRole = loggedInUser.user_metadata.role;
+        return res.status(500).json({ error: e});
     }
-
-    const matchHistory = await matchesData.getTeamRecords();
-
-    res.render("partials/seeding_table", {
-        title: "Seeding Table", 
-        shortcode: 'seedingTable',
-        isAuthenticated: req.oidc.isAuthenticated(),
-        loggedInUser: loggedInUser,
-        role: userRole,
-        allUsers: allUsers,
-        length: allUsers.length,
-        matches: matchHistory,
-        seedNumber: 0,
-    });
 });
 
 router.get("/seedCount", async (req, res) => {
 
-    return res.json(await matchesData.getTeamRecords());
+    try {
+        return res.json(await matchesData.getTeamRecords());
+    } catch (e) {
+        return res.status(500).json({ error: e});
+    }
+});
+
+router.post("/insertSeeds", async (req, res) => {
+
+    try {
+        return res.json("done");
+    } catch (e) {
+        return res.status(500).json({ error: e});
+    }
 });
 
 module.exports = router;
