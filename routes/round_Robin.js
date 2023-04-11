3d3df49fbd0c67b391c742c0cc0ed8a69ee018e2
bkmistry1 <bmistry1@stevens.edu>
2023-04-10 21:54:09 -0400

    created create schedule button to generate round robin schedule

--- routes/round_Robin.js
+++ routes/round_Robin.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
     let allUsers = [];
     let nickname = "";
     let name = "";
-    let rounds;
+    // let rounds;
 
     if(req.oidc.isAuthenticated()) {
         email = req.oidc.user.name;
@@ -22,14 +22,14 @@ router.get("/", async (req, res) => {
         nickname = loggedInUser.email
         userRole = loggedInUser.user_metadata.role;
         name = loggedInUser.user_metadata.name;
-        rounds = await poolsData.roundRobinSelection();        
+        // rounds = await poolsData.roundRobinSelection();        
     }
 
     res.render("partials/round_robin", {
         title: "Round-Robin", 
         shortcode: 'roundRobin',
         isAuthenticated: req.oidc.isAuthenticated(),
-        rounds: rounds,
+        // rounds: rounds,
     });
 });
 
