979ac36d2b1fe88be35d67ce77661eda86239da6
sekwanaa <sk123rulz@gmail.com>
2023-04-12 17:49:55 -0400

    created function to cross out a team **Need to figure out how to pull whether team was loser then I can use that in the function

--- public/js/bracket_view.js
+++ public/js/bracket_view.js
@@ -1,29 +1,36 @@
 (function ($) {
-    // $(":button").click(function() {
-    //     console.log($(this).attr("class"))
-    // }}
-    try {
-        let req = {
-            method: 'POST',
-            url: '/court_view',
-            contentType: 'application/json',
-            data: JSON.stringify({
-                team1: team1,
-                team2: team2,
-                score1: team1Score,
-                score2: team2Score,
-                winner: winner,
-                loser: loser,
-                fieldNum: fieldNum,
-                winnerPointDifferential: winnerPointDifferential,
-                loserPointDifferential: loserPointDifferential,
-            })
+    var playoffTeamName2 = $("#playoffTeam2")[0].innerHTML;
+    let count = 0;
+    $(":button").click(function (event) {
+        count += 1
+        if(count%2==0) {
+            $("#playoffTeam2").replaceWith($('<h5 id="playoffTeam2">'+playoffTeamName2+'</h5>'))
+        } else {
+            $("#playoffTeam2").replaceWith($('<s id="playoffTeam2">'+playoffTeamName2+'</s>'))
         };
-        $.ajax(req).then(function (res) {
+    });
+    // try {
+    //     let req = {
+    //         method: 'POST',
+    //         url: '/court_view',
+    //         contentType: 'application/json',
+    //         data: JSON.stringify({
+    //             team1: team1,
+    //             team2: team2,
+    //             score1: team1Score,
+    //             score2: team2Score,
+    //             winner: winner,
+    //             loser: loser,
+    //             fieldNum: fieldNum,
+    //             winnerPointDifferential: winnerPointDifferential,
+    //             loserPointDifferential: loserPointDifferential,
+    //         })
+    //     };
+    //     $.ajax(req).then(function (res) {
             
-        });
-    } 
-    catch (e) {
-        console.log(e)
-    }
+    //     });
+    // } 
+    // catch (e) {
+    //     console.log(e)
+    // }
 })(window.jQuery);
