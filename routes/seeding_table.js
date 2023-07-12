2de8c4db31da3facdec0cf920cebd39013dbd1f2
bkmistry1 <bmistry1@stevens.edu>
2023-07-12 12:40:37 -0400

    -commenting out original get route -new get route uses id/sport

--- routes/seeding_table.js
+++ routes/seeding_table.js
@@ -6,41 +6,41 @@ const matchesData = data.matchesData;
 const teamsData = data.teamsData;
 const poolsData = data.poolsData;
 
-router.get('/', async (req, res) => {
-	try {
-		let userRole = '';
-		let tournamentJoinedArray = [];
+// router.get('/', async (req, res) => {
+// 	try {
+// 		let userRole = '';
+// 		let tournamentJoinedArray = [];
 
-		if (req.oidc.isAuthenticated()) {
-			const email = req.oidc.user.name;
+// 		if (req.oidc.isAuthenticated()) {
+// 			const email = req.oidc.user.name;
 
-			const user = await userData.getUserByEmail(email);
-			userRole = user.user_metadata.role;
-			const player = await teamsData.getPlayerByUserId(user._id.toString());
-			tournamentJoinedArray = await poolsData.getTournamentJoinedByUser(player._id.toString());
-		}
+// 			const user = await userData.getUserByEmail(email);
+// 			userRole = user.user_metadata.role;
+// 			const player = await teamsData.getPlayerByUserId(user._id.toString());
+// 			tournamentJoinedArray = await poolsData.getTournamentJoinedByUser(player._id.toString());
+// 		}
 
-		const matchHistory = await matchesData.getTeamRecords();
-		const seedsInfo = await poolsData.getAllSeeds();
+// 		const matchHistory = await matchesData.getTeamRecords();
+// 		const seedsInfo = await poolsData.getAllSeeds();
 
-		let isStage1 = true;
-		if (seedsInfo.length > 0) {
-			isStage1 = false;
-		}
+// 		let isStage1 = true;
+// 		if (seedsInfo.length > 0) {
+// 			isStage1 = false;
+// 		}
 
-		res.render('partials/seeding_table', {
-			title: 'Seeding Table',
-			shortcode: 'seedingTable',
-			isAuthenticated: req.oidc.isAuthenticated(),
-			role: userRole,
-			matches: matchHistory,
-			stage1: isStage1,
-			tournamentJoinedArray: tournamentJoinedArray,
-		});
-	} catch (e) {
-		return res.status(500).json({ error: e });
-	}
-});
+// 		res.render('partials/seeding_table', {
+// 			title: 'Seeding Table',
+// 			shortcode: 'seedingTable',
+// 			isAuthenticated: req.oidc.isAuthenticated(),
+// 			role: userRole,
+// 			matches: matchHistory,
+// 			stage1: isStage1,
+// 			tournamentJoinedArray: tournamentJoinedArray,
+// 		});
+// 	} catch (e) {
+// 		return res.status(500).json({ error: e });
+// 	}
+// });
 
 router.get('/:id/:sport', async (req, res) => {
 	let tournamentId = req.params.id;
