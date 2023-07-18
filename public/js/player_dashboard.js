96cbf8be0e5db29c7522e69abc27c27f9bdc73c8
sekwanaa <sk123rulz@gmail.com>
2023-07-18 11:45:03 -0400

    when editing current power ranking, shows current

--- public/js/player_dashboard.js
+++ public/js/player_dashboard.js
@@ -225,7 +225,7 @@
 			teamDisplayCard.hide();
 			var teamMemberName = $(".teamMemberName")
 			for (let i=0;i<teamMemberName[0].children.length;i++) {
-				console.log($('#teamMember' + i)[0].innerHTML.split("-")[0])
+				// console.log($('#teamMember' + i)[0].innerHTML.split("-")[0])
 				editTeamMembersDiv.append(
 					'<input id="editTeamMemberInput' + teamMemberCount + '" type="text">'
 				)
